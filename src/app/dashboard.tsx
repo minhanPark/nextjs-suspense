81f4d5d020a70350d221b3309c765dc501fe753b
Running water <alsgks1218@naver.com>
2025-04-16 11:05:23 +0900

    프로미스를 컴포넌트에 전달 후 use를 사용하고 suspense로 세분화

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,15 +1,27 @@
+import { Suspense, use } from "react";
+
 import { getStocks } from "@/lib/getStocks";
+import { StockInfo } from "@/types";
+
+function StockDisplay({ stockPromise }: { stockPromise: Promise<StockInfo> }) {
+  const { name, ui } = use(stockPromise);
+  return (
+    <div>
+      <div className="font-bold text-3xl">{name}</div>
+      <div>{ui}</div>
+    </div>
+  );
+}
 
 export default async function Dashboard() {
-  const stocks = await Promise.all(await getStocks());
+  const stocks = await getStocks();
 
   return (
     <div className="grid grid-cols-3 gap-4 mt-5">
-      {stocks.map(({ name, ui }) => (
-        <div key={name}>
-          <div className="font-bold text-3xl">{name}</div>
-          <div>{ui}</div>
-        </div>
+      {stocks.map((stockPromise, index) => (
+        <Suspense key={index} fallback={<div>loading ...</div>}>
+          <StockDisplay stockPromise={stockPromise} />
+        </Suspense>
       ))}
     </div>
   );
