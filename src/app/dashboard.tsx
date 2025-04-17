3069e80583e4784ae9a596cf5bc02a20db78a8fd
minhanPark <alsgks1218@naver.com>
2025-04-17 10:01:17 +0900

    서버액션을 통해 데이터를 받아와서 suspense로 세분화

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,7 +1,9 @@
-import { Suspense, use } from "react";
+"use client";
 
-import { getStocks } from "@/lib/getStocks";
+import { Suspense, use, useEffect, useState } from "react";
+
 import { StockInfo } from "@/types";
+import { getStocksAction } from "./actions/getStockAction";
 
 function StockDisplay({ stockPromise }: { stockPromise: Promise<StockInfo> }) {
   const { name, ui } = use(stockPromise);
@@ -13,16 +15,26 @@ function StockDisplay({ stockPromise }: { stockPromise: Promise<StockInfo> }) {
   );
 }
 
-export default async function Dashboard() {
-  const stocks = await getStocks();
+export default function Dashboard() {
+  const [stocks, setStocks] = useState<Promise<StockInfo>[]>([]);
+
+  const callStockActions = async () =>
+    getStocksAction().then((stocks) => setStocks(stocks));
+
+  useEffect(() => {
+    getStocksAction().then((stocks) => setStocks(stocks));
+  }, []);
 
   return (
-    <div className="grid grid-cols-3 gap-4 mt-5">
-      {stocks.map((stockPromise, index) => (
-        <Suspense key={index} fallback={<div>loading ...</div>}>
-          <StockDisplay stockPromise={stockPromise} />
-        </Suspense>
-      ))}
-    </div>
+    <>
+      <button onClick={callStockActions}>클릭</button>
+      <div className="grid grid-cols-3 gap-4 mt-5">
+        {stocks.map((stockPromise, index) => (
+          <Suspense key={index} fallback={<div>loading ...</div>}>
+            <StockDisplay stockPromise={stockPromise} />
+          </Suspense>
+        ))}
+      </div>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Dashboard from "./dashboard";
 
+import StockWithCounter from "@/components/stock-with-counter";
+const foo = StockWithCounter;
+
 export default async function Home() {
   return (
     <main className="text-2xl max-w-3xl mx-auto">
